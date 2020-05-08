01d1b449e1ee980561ff58ec6a9b596fc7b3b6f2
conork033 <46961737+conork033@users.noreply.github.com>
2020-05-08 17:36:27 +0100

    deleteEmployee test

--- src/test/java/ie/gmit/EmployeeListTest.java
+++ src/test/java/ie/gmit/EmployeeListTest.java
@@ -25,4 +25,31 @@ public class EmployeeListTest {
 
         assertEquals(true, NewList.SearchEmployee(e1));
     }
+
+    @Test
+    void addEmployeeDup(){
+        //Test to catch a error Dup of Employee
+        NewList = new EmployeeList();
+        employees e2 = new employees("Conor","kenny","conork@gmail","1234567890",121,"loughrea");
+        NewList.addEmployee(e2);
+
+        Exception e = assertThrows(IllegalArgumentException.class,()->  NewList.addEmployee(e2));
+
+
+        assertEquals("Employee data already exists",e.getMessage());
+    }
+
+    @Test
+    void deleteEmployee(){
+        //test to remove a Employee
+        employees e4 = new employees("Conor","kenny","conork@gmail","1234567890",121,"loughrea");
+        NewList = new EmployeeList();
+        NewList.addEmployee(e4);
+
+
+        assertEquals(true,NewList.SearchEmployee(e4));
+      //  NewList.deleteEmployee(e4);
+        Exception e = assertThrows(IllegalArgumentException.class,()-> NewList.SearchEmployee(e4));
+        assertEquals("Employee data does not exist",e.getMessage());
+    }
 }
