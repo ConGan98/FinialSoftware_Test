1934f790e348b6837d0a228d08c8226c33971ad2
conork033 <46961737+conork033@users.noreply.github.com>
2020-05-08 22:39:05 +0100

    squad number test fix

--- src/test/java/ie/gmit/PlayerDBTest.java
+++ src/test/java/ie/gmit/PlayerDBTest.java
@@ -19,7 +19,7 @@ public class PlayerDBTest {
 
     @Test
     void addEmployee() {
-        players p1 = new players("conor ", "kenny", "conork@gmail", "1234567890", 121, "loughrea");
+        players p1 = new players("conor ", "kenny", "conork@gmail", "1234567890", 6, "loughrea");
         NewList = new PlayerDB();
         NewList.addPlayer(p1);
 
@@ -30,7 +30,7 @@ public class PlayerDBTest {
     void addEmployeeDup(){
         //Test to catch a error Dup of Employee
         NewList = new PlayerDB();
-        players p2 = new players("Conor","kenny","conork@gmail","1234567890",121,"loughrea");
+        players p2 = new players("Conor","kenny","conork@gmail","1234567890",6,"loughrea");
         NewList.addPlayer(p2);
 
         Exception e = assertThrows(IllegalArgumentException.class,()->  NewList.addPlayer(p2));
@@ -42,7 +42,7 @@ public class PlayerDBTest {
     @Test
     void removePlayer(){
         //Test to find if employee exists to remove
-        players p4 = new players("Conor","kenny","conork@gmail","1234567890",121,"loughrea");
+        players p4 = new players("Conor","kenny","conork@gmail","1234567890",6,"loughrea");
         NewList = new PlayerDB();
         Exception e = assertThrows(IllegalArgumentException.class,()-> NewList.deletePlayer(p4));
         assertEquals("player data does not exist",e.getMessage());
@@ -51,7 +51,7 @@ public class PlayerDBTest {
     @Test
     void removePlayerSuccess(){
         //Test to find if employee exists to remove
-        players p4 = new players("Conor","kenny","conork@gmail","1234567890",121,"loughrea");
+        players p4 = new players("Conor","kenny","conork@gmail","1234567890",6,"loughrea");
         NewList = new PlayerDB();
         NewList.addPlayer(p4);
         NewList.deletePlayer(p4);
@@ -61,7 +61,7 @@ public class PlayerDBTest {
 
     @Test
     void searchEmployeeFail(){
-        players p4 = new players("Conor","kenny","conork@gmail","1234567890",121,"loughrea");
+        players p4 = new players("Conor","kenny","conork@gmail","1234567890",6,"loughrea");
         NewList = new PlayerDB();
         Exception e = assertThrows(IllegalArgumentException.class,()-> NewList.SearchPlayer(p4));
         assertEquals("player data does not exist",e.getMessage());
@@ -70,7 +70,7 @@ public class PlayerDBTest {
     @Test
     void cannotFind(){
         NewList = new PlayerDB();
-        players p4 = new players("Conor","kenny","conork@gmail","1234567890",121,"loughrea");
+        players p4 = new players("Conor","kenny","conork@gmail","1234567890",6,"loughrea");
         Exception e = assertThrows(IllegalArgumentException.class,()-> NewList.deletePlayer(p4));
         assertEquals("player data does not exist",e.getMessage());
     }
