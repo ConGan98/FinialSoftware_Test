20e2f76ed14f9b36442ba1ba7fb41c7edbcc1663
conork033 <46961737+conork033@users.noreply.github.com>
2020-05-08 17:19:52 +0100

    addEmployee test

---
+++ src/test/java/ie/gmit/EmployeeListTest.java
@@ -0,0 +1,28 @@
+package ie.gmit;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.EnumMap;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+public class EmployeeListTest {
+
+    private EmployeeList NewList;
+    private employees e1;
+
+    @BeforeEach
+    void init() {
+    }
+
+    @Test
+    void addEmployee() {
+        employees e1 = new employees("Conor", "kenny", "conork@gmail", "1234567890", 121, "loughrea");
+        NewList = new EmployeeList();
+        NewList.addEmployee(e1);
+
+        assertEquals(true, NewList.SearchEmployee(e1));
+    }
+}
